1f1d46bd46d3dca40b5d9b42f8e78c4aebbd7afa
ComputerGuyBobbyJ <ComputerGuyBobbyJ@users.noreply.github.com>
2015-07-26 12:56:26 -0600

    Programming Assignment 2
    Matrix inversion using matrix inversion object cache function and matrix
    inversion calculation using solve() function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix - create cache matrix inverse object
+## cacheSolve - return matrix inverse
+##            - if cached inverse not NULL, return cached result
+##            - otherwise calcuate matrix inverse using solve() and return it
 
-## Write a short comment describing this function
+## create object to cache inverse of matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Create matrix object to cache matrix inverse
+    x.inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        x.inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) x.inverse <<- solve
+    getinverse <- function() x.inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)    
 }
 
 
-## Write a short comment describing this function
+## return inverse of matrix x
+## first see if inverse is cached and return cached inverse,
+## otherwise calculate matrix inverse using solve() and return result
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    x.inverse <- x$getinverse()
+    if(!is.null(x.inverse)) {
+        message("getting cached data")
+        return(x.inverse)
+    }
+    data      <- x$get()    
+    x.inverse <- solve(data, ...)
+    x$setinverse(x.inverse)
+    x.inverse
 }
